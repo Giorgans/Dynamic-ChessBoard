8ec5490ccdc569f0c6db9f2a4be93b04c20090a4
George Zervos <g.zervos96@windowslive.com>
2023-05-27 01:29:11 +0300

    changed number from slider can now be passed to other view controller to create the chessboard

--- Dynamic-ChessBoard/ViewController.swift
+++ Dynamic-ChessBoard/ViewController.swift
@@ -11,15 +11,10 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var text_value: UILabel!
     @IBOutlet weak var numberSelector: UISlider!
-    
-    func passData() -> Int {
-        return Int(numberSelector.value)
-    }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         text_value.text = "Chess Board Dimension: " + String(Int(numberSelector.value)) + " x " + String(Int(numberSelector.value))
-
     }
 
     @IBAction func slider(_ sender: Any) {
@@ -28,41 +23,53 @@ class ViewController: UIViewController {
     }
     
     @IBAction func buttonPressed(_ sender: Any) {
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+        // Get a reference to the second view controller
+        let vc = segue.destination as! ChessBoardViewController
+
+        // Set a variable in the second view controller with the String to pass
+        vc.d =  Int(numberSelector.value)
         
     }
-    
+
 }
 
 
 class ChessBoardViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    
+
     @IBOutlet weak var temp: UILabel!
     private var board: UICollectionView?
-    var d = 16
+    var d : Int = 0
     var count = 0
     var row = 0 , col = 0
     var colorFlag = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
         let layout = UICollectionViewFlowLayout()
-        layout.minimumInteritemSpacing = 0
-        layout.minimumLineSpacing = 0
-        layout.estimatedItemSize = CGSize(width: (359/CGFloat(d))  , height: (359/CGFloat(d)) )
         
         board = UICollectionView(frame: .zero, collectionViewLayout: layout)
         board?.delegate = self
         board?.dataSource = self
         board?.register(ChessBoardCell.self, forCellWithReuseIdentifier: ChessBoardCell.id)
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        layout.estimatedItemSize = CGSize(width: (359/CGFloat(d))  , height: (359/CGFloat(d)) )
+        temp.text = String(d)
+        board?.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(board!)
         
-        board?.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             board!.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             board!.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             board!.heightAnchor.constraint(equalToConstant: 360),
             board!.widthAnchor.constraint(equalToConstant: 360)
         ])
-        
+
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -89,6 +96,11 @@ class ChessBoardViewController: UIViewController, UICollectionViewDelegate, UICo
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return d*d
     }
+    
+    func passDim(dim: Int) {
+        d = dim
+    }
+    
 
 }
 
